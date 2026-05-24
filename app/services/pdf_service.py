6afef0f81a78b13d0f472edad2793426589292f0
Jean Paul Cardozo diaz <paulcardozo_01@outlook.com>
2026-05-24 15:44:59 -0500

    feat(pdf): validate file typ and corrupted PDFs

--- app/services/pdf_service.py
+++ app/services/pdf_service.py
@@ -1,16 +1,28 @@
 from io import BytesIO
 
-from fastapi import UploadFile
+from fastapi import UploadFile, HTTPException
+
 from pypdf import PdfWriter, PdfReader
+from pypdf.errors import PdfReadError
+
 
 
 async def merge_pdfs(files: list[UploadFile]) -> bytes:
     writer = PdfWriter()
 
     for file in files:
+
+        if file.content_type != "application/pdf":
+            raise HTTPException(status_code=400, detail="Only PDF files are allowed")
+
         pdf_bytes = await file.read()
         pdf_stream = BytesIO(pdf_bytes)
-        reader = PdfReader(pdf_stream)
+
+        try:
+            reader = PdfReader(pdf_stream)
+
+        except PdfReadError:
+            raise HTTPException(status_code=400, detail=f"Invalid or corrupted PDF :{file.filename}")
 
         for page in reader.pages:
             writer.add_page(page)
